2f5d30388bf29655440f85182fdc28b6ac24a3b7
mpf <mpf>
2024-02-07 20:41:48 -0500

    sw not working

--- link-pwa/sw.js
+++ link-pwa/sw.js
@@ -3,6 +3,16 @@ var filesToCache = [
   '/link-pwd/index.html'
 ];
 
+// When the service worker is installing, open the cache and add the precache resources to it
+self.addEventListener('install', (event) => {
+  console.log('Service worker install event!');
+  event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(precacheResources)));
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('Service worker activate event!');
+});
+
 /* Start the service worker and cache all of the app's content */
 self.addEventListener('install', function(e) {
   e.waitUntil(
