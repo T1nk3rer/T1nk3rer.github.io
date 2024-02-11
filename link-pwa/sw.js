361f1c39030a6d2c3fc35f38272b0505c156fb12
mpf <mpf>
2024-02-11 10:07:30 -0500

    updating refereneces to make work offline

--- link-pwa/sw.js
+++ link-pwa/sw.js
@@ -1,11 +1,11 @@
 var cacheName = 'link-margin-pwa';
 var filesToCache = [
-  '/link-pwd/index.html'
+  './index.html'
 ];
 
 /* Start the service worker and cache all of the app's content */
 self.addEventListener('install', function(e) {
-  console.log('Service worker install event!');
+  //console.log('Service worker install event!');
   e.waitUntil(
     caches.open(cacheName).then(function(cache) {
       return cache.addAll(filesToCache);
@@ -15,11 +15,12 @@ self.addEventListener('install', function(e) {
 });
 
 self.addEventListener('activate', (event) => {
-  console.log('Service worker activate event!');
+  //console.log('Service worker activate event!');
 });
 
 /* Serve cached content when offline */
 self.addEventListener('fetch', function(e) {
+  //console.log('Service worker fetch event!');
   e.respondWith(
     caches.match(e.request).then(function(response) {
       return response || fetch(e.request);
